72c1587d7e8fbced96c9d66bff1facd804107414
PizzaLovingNerd <pizzalovingnerd@teknik.io>
2022-02-28 22:38:29 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+//Code written by TheBaconPug
+const text = document.getElementById("info-text");
+const button = document.getElementById("warning-continue");
+
+button.style.display = "none";
+
+const queryString = window.location.search;
+
+const urlParams = new URLSearchParams(queryString);
+let url = urlParams.get("url");
+
+if (!url) {
+  text.innerHTML = "No URL specified";
+}
+
+if (!url.startsWith("http")) {
+  url = "https://" + url.split("/")[0];
+} else {
+  url = "https://" + url.split("/")[2];
+}
+
+YAML.load("list.yml", function (result) {
+  let list = YAML.parse(YAML.stringify(result));
+  if (!list[url]) return window.location.replace(url);
+  for (i in list[url]) {
+    if (i == "redirect") {
+      window.location.replace(list[url][i]);
+    } else if (i == "warning") {
+      text.innerHTML = `${i} reason: ${list[url][i]}`;
+      button.style.display = "block";
+      button.innerHTML = "Continue anyway";
+      button.addEventListener("click", function () {
+        window.location.replace(url);
+      });
+    } else {
+      text.innerHTML = `${i} reason: ${list[url][i]}`;
+    }
+  }
+});
